875821bb2a2c0f32a2331495fa7a68ca43743086
Bela <belahamid@gmail.com>
2021-01-03 23:24:07 +0000

    test passed: card_has_value

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,11 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.card1 = Card("Clubs", 1)
+        self.card2 = Card("Clubs", 2)
+
+    def test_card_has_value(self):
+        self.assertEqual(2, self.card2.value)
+    
